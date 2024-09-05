f2f81ba368e99a96a21fa0940619f212df521463
Fabio Salomao <fabio@arquivarnet.com.br>
2024-09-05 10:53:15 -0500

    feat: readequação do projeto

--- OrderService/Program.cs
+++ OrderService/Program.cs
@@ -1,18 +1,17 @@
-using OrderService.Test;
+using Kafka.Test;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+//Teste do Kafka
 KafkaTest.Execute();
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
